f08a4367bade75251e6982bcb8905d2658e5aa76
einnuj <junniesim91+openSource@gmail.com>
2017-08-27 04:43:03 -0400

    RestService is fully implemented - should test.

--- src/main/java/com/silkroad/model/service/RestService.java
+++ src/main/java/com/silkroad/model/service/RestService.java
@@ -1,5 +1,6 @@
 package com.silkroad.model.service;
 
+import java.io.IOException;
 import java.util.Map;
 
 public interface RestService {
@@ -8,16 +9,18 @@ public interface RestService {
      * Returns a String response from executing a GET request on target argument String url.
      *
      * @param url a string representing the URL from which to execute a GET request
+     * @throws IOException
      * @return a String response from executing a GET request on the target argument String url.
      */
-    String doGetRequest(String url);
+    String doGetRequest(String url) throws IOException;
 
     /**
      * Returns a String response from executing a POST request on target argument String url with a Map of parameters.
      *
      * @param url a string representing the URL from which to execute a GET request
      * @param parameters a map with String key, String value representing the parameters of a POST request
+     * @throws IOException
      * @return a String response from executing a POST request on the target argument String url with the given parameters
      */
-    String doPostRequest(String url, Map<String, String> parameters);
+    String doPostRequest(String url, Map<String, String> parameters) throws IOException;
 }
--- src/main/java/com/silkroad/model/service/RestServiceDefaultImpl.java
+++ src/main/java/com/silkroad/model/service/RestServiceDefaultImpl.java
@@ -1,16 +1,41 @@
 package com.silkroad.model.service;
 
+import com.google.gson.Gson;
+import okhttp3.*;
+
+import java.io.IOException;
 import java.util.Map;
 
 public class RestServiceDefaultImpl implements RestService {
 
-    @Override
-    public String doGetRequest(String url) {
-        return "";
+    private static final String CONTENT_TYPE_JSON = "application/json; charset=utf-8";
+
+    public String doGetRequest(String url) throws IOException{
+        final OkHttpClient httpClient = new OkHttpClient();
+
+        Request getRequest = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = httpClient.newCall(getRequest).execute();
+
+        return response.body().string();
     }
 
-    @Override
-    public String doPostRequest(String url, Map<String, String> parameters) {
-        return "";
+    public String doPostRequest(String url, Map<String, String> parameters) throws IOException {
+        final OkHttpClient httpClient = new OkHttpClient();
+
+        Gson gson = new Gson();
+        String jsonParameters = gson.toJson(parameters);
+
+        MediaType mediaTypeJSON = MediaType.parse(CONTENT_TYPE_JSON);
+        RequestBody requestBody = RequestBody.create(mediaTypeJSON, jsonParameters);
+
+        Request postRequest = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+        Response response = httpClient.newCall(postRequest).execute();
+
+        return response.body().string();
     }
 }
