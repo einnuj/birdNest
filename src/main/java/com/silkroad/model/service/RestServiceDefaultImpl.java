f99e96800f85fa342baad568b7692b2da544ff23
einnuj <junniesim91+openSource@gmail.com>
2017-08-27 01:50:32 -0400

    Skeleton of RestServiceDefaultImpl

---
+++ src/main/java/com/silkroad/model/service/RestServiceDefaultImpl.java
@@ -0,0 +1,16 @@
+package com.silkroad.model.service;
+
+import java.util.Map;
+
+public class RestServiceDefaultImpl implements RestService {
+
+    @Override
+    public String doGetRequest(String url) {
+        return "";
+    }
+
+    @Override
+    public String doPostRequest(String url, Map<String, String> parameters) {
+        return "";
+    }
+}
